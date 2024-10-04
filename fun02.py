9309c3b0f85c5eb5ca35e286ea3f9461a32668f8
joaopaulo-oss <joaopaulo24973@gmail.com>
2024-10-04 04:41:56 -0300

    função par ou impar

---
+++ fun02.py
@@ -0,0 +1,11 @@
+# Crie uma função chamada verificar_paridade que receba um número inteiro como argumento e retorne uma mensagem indicando se o número é par ou ímpar.
+def verificar_paridade(numero):
+    if numero % 2 ==0:
+        print(f"numero é par!")
+    else:
+        print(f"numero é impar!")
+        
+        
+          
+        
+
